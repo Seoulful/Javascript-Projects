b174729cf304a3b0fd00396ad95445c6791a38ef
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-04-11 16:17:20 -0700

    added project2 folder and files

---
+++ Basic JavaSript Projects/project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function my_first_function() {
+    var str = "This is the button text!";
+    document.getElementById("button_text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "I am concatenating ";
+    sentence += "these two strings.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
