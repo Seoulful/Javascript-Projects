f0a8d21ed6c592522340d09d32bf997e6a5e9edd
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-04-11 14:59:09 -0700

    added index and main.js

---
+++ Basic JavaSript Projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello, World!") //adds an alert window that prints "Hello, World!"//
+
+document.write("Hello, World!") //prints "Hello, World!"// 
+
+var a = "This is my string."; //assigns variable a to the string: "This is my string."//
+
+document.write(a); //prints variable a//
+
+window.alert(a); //adds an alert window that prints variable a//
+
+document.write("\"This is in quotation marks\""); //example of a string with quotation marks//
+
+document.write("I am concatenating this" + " with this."); //example of concatenating two strings//
+
+var b = "concatenated" + "string"; //assigns variable b to two strings to be concatenated//
+
+document.write(b); //prints variable b//
+
+var c = "apples", d = "oranges", e = "grapes"; //assigns strings to variables c, d, and e//
+
+document.write(d); //prints variable d//
+
+document.write(3+3); //prints the answer to the equation//
+
+var f = "green";
+
+document.write(f.fontcolor("green"));
