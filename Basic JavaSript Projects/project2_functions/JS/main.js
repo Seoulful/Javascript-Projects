61d574318150acda1ed2c0cdec0d2d29de6243f6
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-04-11 16:22:46 -0700

    added comments to main.js

--- Basic JavaSript Projects/project2_functions/JS/main.js
+++ Basic JavaSript Projects/project2_functions/JS/main.js
@@ -1,10 +1,10 @@
-function my_first_function() {
-    var str = "This is the button text!";
-    document.getElementById("button_text").innerHTML = str;
+function my_first_function() { //assigns a name to the function//
+    var str = "This is the button text!"; //assigns the variable "str" with a string//
+    document.getElementById("button_text").innerHTML = str; //tells the script to run when the specified id is called//
 }
 
-function myFunction() {
-    var sentence = "I am concatenating ";
+function myFunction() { //assigns a name to the function//
+    var sentence = "I am concatenating "; //assigns the variable "sentence" with two strings to concatenate//
     sentence += "these two strings.";
-    document.getElementById("Concatenate").innerHTML = sentence;
+    document.getElementById("Concatenate").innerHTML = sentence; //tells the script to run when the specified id is called//
 }
